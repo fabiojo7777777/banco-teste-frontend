86c6817e76d3d017fb19087c56a75fac4740a823
Fabio Silva <fabiojo7777777@gmail.com>
2020-08-05 07:21:12 -0300

    altering junit test

--- spec/conta/conta.controller.spec.js
+++ spec/conta/conta.controller.spec.js
@@ -3,18 +3,17 @@ describe("conta.controller", function() {
     'use strict';
 
     var ctrl;
-    var _$scope_;
 
     beforeEach(module("conta-controllers"));
 
     beforeEach(module(function($provide, $rootScopeProvider) {
         // mock do backend
         $provide.factory("BackendService", SuperMock.mockarBackend());
-        _$scope_ = $rootScopeProvider.$get().$new();
-        $provide.factory("$scope", function() { return _$scope_; });
+		// mock do $scope
+        $provide.factory("$scope", function() { return $rootScopeProvider.$get().$new(); });
     }));
 
-    beforeEach(inject(function(_$controller_, _$rootScope_) {
+    beforeEach(inject(function(_$controller_) {
         ctrl = _$controller_("ContaController");
     }));
 
@@ -22,7 +21,7 @@ describe("conta.controller", function() {
         SuperMock.verificarNenhumProcessoAssincronoDoAngularPendenteDeExecucao();
     });
 
-    it("*** Testar ContaController ***", inject(function(_$rootScope_) {
+    it("*** Testar ContaController ***", inject(function(_$rootScope_, _$scope_) {
         _$rootScope_.messages = [];
 
         SuperMock.mockarRespostaBackend("contas", undefined, Api.erros.CONTAS_DEU_ERRO_1);
